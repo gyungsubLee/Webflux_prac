0328edb4e74bde39712e88a1b770049ec9a81925
gyoungsubLee <dltjq2323@gmail.com>
2025-06-13 20:16:50 +0900

    project init

---
+++ src/test/java/webflux/prac/PracApplicationTests.java
@@ -0,0 +1,13 @@
+package webflux.prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
